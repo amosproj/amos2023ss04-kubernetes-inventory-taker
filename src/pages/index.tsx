3bcf6772349f19a7c61c82331a08b86a4cb47f9b
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-05-01 23:44:08 +0200

    Setup Next.js Frontend with API Routes, TailwindCSS, TypeScript

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-2xl font-semibold text-[#326ce5] mt-6 text-center">Kubernetes Inventory Taker</h1>
+      <p className="text-center">by the way: #326ce5 is the hex code of the kubernetes blue color ...</p>
+    </>
+  )
+}
