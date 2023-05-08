c9327042f73e1ec10151c6735c800ac01831e4fd
freefreeP <66095628+freefreeP@users.noreply.github.com>
2023-05-08 21:54:27 +0200

    table for Node
    Signed-off-by: freefreeP <66095628+freefreeP@users.noreply.github.com>

--- kubernetes-schema.sql
+++ kubernetes-schema.sql
@@ -2,3 +2,12 @@ CREATE TABLE "Cluster" (
   "cluster_id" int PRIMARY KEY,
   "name" varchar(255)
 );
+
+CREATE TABLE "Node" (
+  "node_id" int PRIMARY KEY,
+  "cluster_id" int,
+  "name" varchar(255),
+  "ip_address_internal" varchar(255),
+  "ip_address_external" varchar(255),
+  "status" varchar(50)
+);
