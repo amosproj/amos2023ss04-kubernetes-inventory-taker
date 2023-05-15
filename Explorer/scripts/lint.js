c3ce62b40ba6c6eeb52143dfab250abc7fddd98a
njochens <nikolas@njochens.de>
2023-05-15 22:51:02 +0200

    feat(Lint): javascript prettier pre-commit hooks
    Signed-off-by: njochens <nikolas@njochens.de>
    Signed-off-by: vringar <zabka@campus.tu-berlin.de>

--- Explorer/scripts/lint.js
+++ Explorer/scripts/lint.js
@@ -2,25 +2,25 @@
 // path of the js files to not have frontend/ in the path
 
 const filesToLint = process.argv
-    .slice(2)
-    .map(path => path.replace(/^Explorer\//, ""));
+  .slice(2)
+  .map((path) => path.replace(/^Explorer\//, ""));
 
 const { ESLint } = require("eslint");
 
 (async function main() {
-    // 1. Create an instance.
-    const eslint = new ESLint({fix: true});
+  // 1. Create an instance.
+  const eslint = new ESLint({ fix: true });
 
-    // 2. Lint files.
-    const results = await eslint.lintFiles(filesToLint);
+  // 2. Lint files.
+  const results = await eslint.lintFiles(filesToLint);
 
-    // 3. Format the results.
-    const formatter = await eslint.loadFormatter("stylish");
-    const resultText = formatter.format(results);
+  // 3. Format the results.
+  const formatter = await eslint.loadFormatter("stylish");
+  const resultText = formatter.format(results);
 
-    // 4. Output it.
-    console.log(resultText);
+  // 4. Output it.
+  console.log(resultText);
 })().catch((error) => {
-    process.exitCode = 1;
-    console.error(error);
+  process.exitCode = 1;
+  console.error(error);
 });
