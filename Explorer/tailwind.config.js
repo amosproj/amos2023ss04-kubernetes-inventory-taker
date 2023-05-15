a538e04df24bbf537f18da74c3ef8291bf5951d6
njochens <nikolas@njochens.de>
2023-05-15 12:17:06 +0200

    feat(Explorer): initial container details page
    Signed-off-by: njochens <nikolas@njochens.de>

--- Explorer/tailwind.config.js
+++ Explorer/tailwind.config.js
@@ -4,6 +4,8 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/flowbite/**/*.js',
+    "./node_modules/flowbite-react/**/*.js",
   ],
   theme: {
     extend: {
@@ -14,5 +16,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  //darkMode: 'class',
 }
