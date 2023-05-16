091bde9126a3e446b8fd6fb83428b55bc6dad68f
muhdfahadali <muhdfahadali@gmail.com>
2023-05-16 17:26:08 +0200

    feat(Explorer): added component to detail page

--- Explorer/src/components/container_detail_page.tsx
+++ Explorer/src/components/container_detail_page.tsx
@@ -9,7 +9,6 @@ import {
 export default function Index(): JSX.Element {
     return (
         <div className="p-6">
-            {/*<DarkThemeToggle />*/}
             <ContainerDetailPage />
         </div>
     );
@@ -57,6 +56,14 @@ const container_data = [
     { field: "Restart Option", content: "Always" },
 ];
 
+// Example for Changelog Data Structure to generate page
+const changelog_data = [
+    { status: "running", name: "nginx-earthlt", port:'8000', started:'15 minutes ago' },
+    { status: "running", name: "kind bouman", port:'3000', started:'10 minutes ago' },
+    { status: "disabled", name: "radis-stack", port:'8000', started:'' },
+    
+];
+
 function ContainerDetailPage(): JSX.Element {
 
     function get_container_status_color() {
@@ -87,10 +94,49 @@ function ContainerDetailPage(): JSX.Element {
                 <div className="w-1/4 w-max">
                     <ContainerDetailsWidget/>
                 </div>
-                <div className="w-1/4 w-max px-8">
+                <div className="w-1/2 w-max px-8">
+                    <ContainerWorkLoad/>
                     <ContainerChangelogWidget />
                 </div>
             </div>
+        </div>
+    );
+}
+
+function ContainerWorkLoad(): JSX.Element {
+    return (
+        <div className="flex row">
+
+            <div className="pr-4">
+                <header>
+                        <h3 className="mt-2 mb-3 text-2xl font-bold">
+                            CPU Work Load 
+                        </h3>
+                </header>
+                <div>
+                        <a href="#" className="block max-w-xs p-12 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CPU Usage</h5>
+                            <p className="font-normal text-gray-700 text-center dark:text-gray-400">15.0%</p>
+                        </a>
+                </div>
+        
+            </div>
+
+            <div className="pl-4">
+                <header>
+                        <h4 className="mt-2 mb-3 text-2xl font-bold">
+                            Memory Work Load   
+                        </h4>
+                </header>
+                <div>
+                        <a href="#" className="block max-w-xs p-12 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Memory Usage</h5>
+                            <p className="font-normal text-gray-700 text-center dark:text-gray-400">60.08%</p>
+                        </a>
+                </div>
+                
+            </div>
+
         </div>
     );
 }
@@ -104,7 +150,26 @@ function ContainerChangelogWidget(): JSX.Element {
                         Changelog
                     </h2>
                 </header>
-                    <div>
+                    <div className="">
+                            <Table>
+                                <Table.Head>
+                                    <Table.HeadCell className="!py-2 bg-gray-50 dark:bg-gray-800">Status</Table.HeadCell>
+                                    <Table.HeadCell className="!py-2 bg-gray-30 dark:bg-gray-600">Name</Table.HeadCell>
+                                    <Table.HeadCell className="!py-2 bg-gray-30 dark:bg-gray-600">Port</Table.HeadCell>
+                                    <Table.HeadCell className="!py-2 bg-gray-30 dark:bg-gray-600">Started</Table.HeadCell>
+                                </Table.Head>
+                                <Table.Body className="divide-y">
+                                    {changelog_data.map((entry, index) => (
+                                        <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                            <Table.Cell className="!py-1  whitespace-nowrap font-medium bg-gray-50 dark:bg-gray-800">{ entry.status=="running"?<span className="flex w-3 h-3 bg-green-500 rounded-full"></span>:<span className="flex w-3 h-3 bg-red-500 rounded-full"></span> }</Table.Cell>
+                                            <Table.Cell className="!py-1 whitespace-nowrap font-medium bg-gray-30 dark:bg-gray-600">{ entry.name }</Table.Cell>
+                                            <Table.Cell className="!py-1 whitespace-nowrap font-medium bg-gray-30 dark:bg-gray-600">{ entry.port }</Table.Cell>
+                                            <Table.Cell className="!py-1 whitespace-nowrap font-medium bg-gray-30 dark:bg-gray-600">{ entry.started }</Table.Cell>
+                                        </Table.Row>
+                                    ))}
+                                </Table.Body>
+                            </Table> 
+                        
                     </div>
             </section>
         </div>
