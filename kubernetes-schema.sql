e0deb9f5a025738967e96df53f2021e15044609d
njochens <nikolas@njochens.de>
2023-05-21 23:43:00 +0200

    feat(SQL): added scheme overview and fixed errors in sql file
    Signed-off-by: njochens <nikolas@njochens.de>

--- kubernetes-schema.sql
+++ kubernetes-schema.sql
@@ -1,38 +1,38 @@
 CREATE TABLE "Cluster" (
   "cluster_event_id" int PRIMARY KEY,
   "cluster_id" int,
-  "timestamp" datetime NOT NULL;
-  "name" varchar(255)
+  "timestamp" timestamp NOT NULL,
+  "name" varchar(255),
 );
 
 -- https://kubernetes.io/docs/concepts/architecture/nodes/
 CREATE TABLE "Node" (
   "node_event_id" int PRIMARY KEY,
   "node_id" int,
-  "timestamp" datetime NOT NULL;
+  "timestamp" timestamp NOT NULL,
   "cluster_id" int,
   "name" varchar(255),
   "ip_address_internal" varchar(255),
   "ip_address_external" varchar(255),
-  "status" varchar(50)
+  "status" varchar(50),
 );
 
 -- https://kubernetes.io/docs/concepts/workloads/pods/
 CREATE TABLE "Pod" (
   "pod_event_id" int PRIMARY KEY,
   "pod_id" int,
-  "timestamp" datetime NOT NULL;
+  "timestamp" timestamp NOT NULL,
   "cluster_id" int,
   "node_id" int,
   "name" varchar(255),
-  "status" varchar(50)
+  "status" varchar(50),
 );
 
 -- https://kubernetes.io/docs/concepts/containers/
 CREATE TABLE "Container" (
   "container_event_id" int PRIMARY KEY,
   "container_id" int,
-  "timestamp" datetime NOT NULL;
+  "timestamp" timestamp NOT NULL,
   "pod_id" int,
   "name" varchar(255),
   "image" varchar(255),
@@ -46,7 +46,7 @@ CREATE TABLE "Service" (
   "cluster_id" int,
   "name" varchar(255),
   "type" varchar(50),
-  "port" int
+  "port" int,
 );
 
 ALTER TABLE "Service" ADD FOREIGN KEY ("cluster_id") REFERENCES "Cluster" ("cluster_id");
