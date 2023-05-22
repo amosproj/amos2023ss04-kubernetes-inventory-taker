ea55d5c8f945c9668bcaff8117e49cb320ab8fd7
njochens <nikolas@njochens.de>
2023-05-22 13:32:13 +0200

    fix(SQL): fixed error in sql file
    Signed-off-by: njochens <nikolas@njochens.de>

--- kubernetes-schema.sql
+++ kubernetes-schema.sql
@@ -1,12 +1,12 @@
-CREATE TABLE "Cluster" (
+CREATE TABLE "cluster" (
   "cluster_event_id" int PRIMARY KEY,
   "cluster_id" int,
   "timestamp" timestamp NOT NULL,
-  "name" varchar(255),
+  "name" varchar(255)
 );
 
 -- https://kubernetes.io/docs/concepts/architecture/nodes/
-CREATE TABLE "Node" (
+CREATE TABLE "nodes" (
   "node_event_id" int PRIMARY KEY,
   "node_id" int,
   "timestamp" timestamp NOT NULL,
@@ -14,22 +14,22 @@ CREATE TABLE "Node" (
   "name" varchar(255),
   "ip_address_internal" varchar(255),
   "ip_address_external" varchar(255),
-  "status" varchar(50),
+  "status" varchar(50)
 );
 
 -- https://kubernetes.io/docs/concepts/workloads/pods/
-CREATE TABLE "Pod" (
+CREATE TABLE "pods" (
   "pod_event_id" int PRIMARY KEY,
   "pod_id" int,
   "timestamp" timestamp NOT NULL,
   "cluster_id" int,
   "node_id" int,
   "name" varchar(255),
-  "status" varchar(50),
+  "status" varchar(50)
 );
 
 -- https://kubernetes.io/docs/concepts/containers/
-CREATE TABLE "Container" (
+CREATE TABLE "containers" (
   "container_event_id" int PRIMARY KEY,
   "container_id" int,
   "timestamp" timestamp NOT NULL,
@@ -37,16 +37,14 @@ CREATE TABLE "Container" (
   "name" varchar(255),
   "image" varchar(255),
   "status" varchar(50),
-  "ports" varchar(255),
+  "ports" varchar(255)
 );
 
 -- https://kubernetes.io/docs/concepts/services-networking/service/
-CREATE TABLE "Service" (
+CREATE TABLE "services" (
   "service_id" int PRIMARY KEY,
   "cluster_id" int,
   "name" varchar(255),
   "type" varchar(50),
-  "port" int,
+  "port" int
 );
-
-ALTER TABLE "Service" ADD FOREIGN KEY ("cluster_id") REFERENCES "Cluster" ("cluster_id");
