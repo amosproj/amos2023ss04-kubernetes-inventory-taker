2153a9b7d201a1c47eb1d649bb8556aa621f015d
vringar <zabka@campus.tu-berlin.de>
2023-05-24 11:56:53 +0200

    fix(index): display new index page
    Signed-off-by: smnws <117300309+smnws@users.noreply.github.com>
    Signed-off-by: vringar <zabka@campus.tu-berlin.de>

--- Explorer/src/pages/index.tsx
+++ Explorer/src/pages/index.tsx
@@ -1,24 +1,15 @@
-import useSWR from "swr";
+// pages/index.tsx
+import React from "react";
+import StripedTable from "../components/containerTable";
+import containers from "../components/containerTestData";
 
-const fetcher = (url: RequestInfo | URL) =>
-  fetch(url).then((res) => res.json());
-
-export default function Home() {
-  const { data, error, isLoading } = useSWR("/api/hello", fetcher);
-
-  if (error) return <div>failed to load</div>;
-  if (isLoading) return <div>loading...</div>;
+const ContainerPage: React.FC = () => {
   return (
-    <>
-      <h1 className="text-2xl font-semibold text-[#326ce5] mt-6 text-center">
-        Kubernetes Inventory Taker
-      </h1>
-      <p className="text-center">
-        by the way: #326ce5 is the hex code of the kubernetes blue color ...
-      </p>
-      <p className="text-center">
-        Fetched data coming from Backend: {data.name}
-      </p>
-    </>
+    <div className="container mx-auto px-4">
+      <h1 className="text-5xl mb-6 mt-10">Containers</h1>
+      <StripedTable containers={containers} />
+    </div>
   );
-}
+};
+
+export default ContainerPage;
