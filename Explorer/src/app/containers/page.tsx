dcaf2f75ba77d8107f11530c3b77c883ae7ef7d4
njochens <nikolas@njochens.de>
2023-06-13 18:00:56 +0200

    fix(): repaired containers page
    Signed-off-by: njochens <nikolas@njochens.de>

--- Explorer/src/app/containers/page.tsx
+++ Explorer/src/app/containers/page.tsx
@@ -1,18 +1,15 @@
-"use client";
-
+import ContainerTable from "@/components/containerTable";
+import { getContainerList } from "@/lib/db";
 import { H1 } from "@/components/style_elements";
-import React from "react";
-import StripedTable from "@/components/containerTable";
-import containers from "@/components/containerTestData";
-//import { useNavigationStore } from "@/context/sidebar_context";
 
-export default async function Index(): Promise<JSX.Element> {
-  //useNavigationStore((state) => state.changePage("nodes"));
-
+export const dynamic = "force-dynamic";
+/* @ts-expect-error Async Server Component */
+export default async function Index(): JSX.Element {
+  const data = await getContainerList();
   return (
-    <div className="p-6">
+    <div className="container mx-auto px-4">
       <H1 content={"Containers"} />
-      <StripedTable containers={containers} />
+      <ContainerTable list={data} />
     </div>
   );
 }
