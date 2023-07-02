9478b94ec7256c0206a36fd2b64135b18c193a30
lifesbest23 <l.gehrlein@campus.tu-berlin.de>
2023-07-02 20:52:14 +0200

    feat: added persistent volume claim processing
    chore: added util.go for type conversion helper functions
    
    Signed-off-by: lifesbest23 <l.gehrlein@campus.tu-berlin.de>

--- Proxy/internal/cluster/pv_process.go
+++ Proxy/internal/cluster/pv_process.go
@@ -2,13 +2,10 @@ package cluster
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	model "github.com/amosproj/amos2023ss04-kubernetes-inventory-taker/Proxy/internal/database/model"
 	"github.com/uptrace/bun"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
 )
 
@@ -55,33 +52,4 @@ func ProcessPersistentVolume(event Event, bunDB *bun.DB) {
 	if err != nil {
 		klog.Error(err)
 	}
-}
-
-func packTimestamp(timestamp *v1.Time) time.Time {
-	if timestamp == nil {
-		return time.Time{}
-	}
-
-	return timestamp.Time
-}
-
-func packAccessModes(accessModes []corev1.PersistentVolumeAccessMode) []string {
-	ret := make([]string, 0, len(accessModes))
-
-	for _, accessMode := range accessModes {
-		ret = append(ret, string(accessMode))
-	}
-
-	return ret
-}
-
-func packLabels(labels map[string]string) []string {
-	ret := make([]string, 0, len(labels))
-
-	for k, v := range labels {
-		combined := fmt.Sprintf("%s=%s", k, v)
-		ret = append(ret, combined)
-	}
-
-	return ret
 }
