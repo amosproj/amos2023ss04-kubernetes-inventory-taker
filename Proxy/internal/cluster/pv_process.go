c887af6fe1b8673cc2b460178b0361da4c2b57d2
lifesbest23 <l.gehrlein@campus.tu-berlin.de>
2023-07-02 20:37:29 +0200

    feat: added persistent volume processing
    Signed-off-by: lifesbest23 <l.gehrlein@campus.tu-berlin.de>

---
+++ Proxy/internal/cluster/pv_process.go
@@ -0,0 +1,87 @@
+package cluster
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	model "github.com/amosproj/amos2023ss04-kubernetes-inventory-taker/Proxy/internal/database/model"
+	"github.com/uptrace/bun"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog"
+)
+
+func ProcessPersistentVolume(event Event, bunDB *bun.DB) {
+	pvNew, ok := event.Object.(*corev1.PersistentVolume)
+	if !ok {
+		klog.Errorf("unexpected type: %T", event.Object)
+		return
+	}
+
+	// in case of update, check if the resource version is the same
+	if event.Type == Update {
+		pwOld, ok := event.OldObj.(*corev1.PersistentVolume)
+		if !ok {
+			klog.Errorf("unexpected old type: %T", event.OldObj)
+			return
+		}
+
+		if pwOld.ResourceVersion == pvNew.ResourceVersion {
+			return
+		}
+	}
+
+	pvEntry := &model.PersistentVolume{
+		Timestamp:         event.timestamp,
+		Labels:            packLabels(pvNew.Labels),
+		Name:              pvNew.Name,
+		Namespace:         pvNew.Namespace,
+		UID:               string(pvNew.UID),
+		CreationTimestamp: pvNew.CreationTimestamp.Time,
+		DeletionTimestamp: packTimestamp(pvNew.DeletionTimestamp),
+		AccessModes:       packAccessModes(pvNew.Spec.AccessModes),
+		Capacity:          pvNew.Spec.Capacity.Storage().String(),
+		MountOptions:      pvNew.Spec.MountOptions,
+		StorageClassName:  pvNew.Spec.StorageClassName,
+		VolumeMode:        string(*pvNew.Spec.VolumeMode),
+		Message:           pvNew.Status.Message,
+		Phase:             string(pvNew.Status.Phase),
+		Reason:            pvNew.Status.Reason,
+	}
+
+	// Insert the PersistentVolume into the database
+	_, err := bunDB.NewInsert().Model(pvEntry).Exec(context.Background())
+	if err != nil {
+		klog.Error(err)
+	}
+}
+
+func packTimestamp(timestamp *v1.Time) time.Time {
+	if timestamp == nil {
+		return time.Time{}
+	}
+
+	return timestamp.Time
+}
+
+func packAccessModes(accessModes []corev1.PersistentVolumeAccessMode) []string {
+	ret := make([]string, 0, len(accessModes))
+
+	for _, accessMode := range accessModes {
+		ret = append(ret, string(accessMode))
+	}
+
+	return ret
+}
+
+func packLabels(labels map[string]string) []string {
+	ret := make([]string, 0, len(labels))
+
+	for k, v := range labels {
+		combined := fmt.Sprintf("%s=%s", k, v)
+		ret = append(ret, combined)
+	}
+
+	return ret
+}
