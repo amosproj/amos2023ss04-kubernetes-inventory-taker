b4918c345587c1114a322e124103d360dffcf285
Ali Aziri <ali.aziri@prestatech.com>
2023-07-04 01:09:03 +0200

    feat: pods page
    Signed-off-by: Ali Aziri <ali.aziri@prestatech.com>

--- Explorer/src/app/pods/page.tsx
+++ Explorer/src/app/pods/page.tsx
@@ -1,10 +1,15 @@
+import PodsTable from "@/components/podsTable";
 import { H1 } from "@/components/style_elements";
+import { getPodsList } from "@/lib/db";
 
 /* @ts-expect-error Async Server Component */
 export default async function Index(): JSX.Element {
+  const data = await getPodsList();
+
   return (
     <div className="p-6">
       <H1 content={"Pods"} />
+      <PodsTable list={data} />
     </div>
   );
 }
