9b84de80ea9c5d7190a40f1a7515c6474e7365aa
Ali Aziri <ali.aziri@prestatech.com>
2023-07-07 11:34:16 +0200

    feat: added pod property
    Signed-off-by: Ali Aziri <ali.aziri@prestatech.com>

--- Explorer/src/lib/types/Pod.ts
+++ Explorer/src/lib/types/Pod.ts
@@ -1,7 +1,10 @@
+import { Status } from "@/lib/types/Status";
+
 export type Pod = {
   id: number;
   name: string;
   namespace: string;
+  status_phase: Status;
 };
 
 export type PodList = Pod[];
